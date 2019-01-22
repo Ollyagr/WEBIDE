40290cafde218449067a5b2e8eab50a9df4767bc
Oleg Agranovich <oleg.agranovich@cpars.de>
2019-01-22 13:45:34 +0000

    some js changes

--- webapp/testJS/myScript.js
+++ webapp/testJS/myScript.js
@@ -1,8 +1,21 @@
- $(document).ready(function () {
- 	var listItems = $("#ulID").children();
- 	listItems.each(function (i) {
- 		if (this.textContent.includes("3")) {
- 			this.className = "yellow";
- 		}
+ 	function sleep(milliseconds) {
+ 		return new Promise(resolve => setTimeout(resolve, milliseconds));
+ 	}
+ 	$(document).ready(function () {
+
+ 		var listItems = $("#ulID").children();
+ 		listItems.each(function (i) {
+ 			if (this.textContent.includes("3")) {
+ 				this.className = "yellow";
+ 			}
+ 			if (this.textContent.includes("remote")) {
+ 				this.style.border = "1px solid #000";
+ 				this.onclick = function () {
+ 					this.style.display = "inline";
+ 					sleep(2000).then(() => {
+ 						this.style.display = "block";
+ 					});
+ 				};
+ 			}
+ 		});
  	});
- });
